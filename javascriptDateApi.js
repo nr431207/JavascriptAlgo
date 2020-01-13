74dc23263c4240b513f6bfbb830d60e9a1081f22
Neda Rohani <sonia@Nedas-MacBook-Pro.local>
2020-01-13 12:05:20 -0800

    create function to find second Tuesdays of Oct

---
+++ javascriptDateApi.js
@@ -0,0 +1,18 @@
+// working with Date api's
+
+// getting the second Tuesday of October in any given year
+const getSecondTuesday = year => {
+  if(year.toString().length !== 4) {
+    return 'year is invalid';
+  }
+  let newDate = new Date(year, 09);
+  let tuesdays = [];
+  for(let i = 1; i < 32; i += 1) {
+    if(newDate.getDay(newDate.setDate(i)) === 2) {
+      tuesdays.push(new Date(year, 09, i))
+    }
+  }
+  return tuesdays[1];
+}
+
+console.log(getSecondTuesday('2020'))
